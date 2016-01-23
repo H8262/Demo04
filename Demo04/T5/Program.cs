49c7dd5e34861e8e77340cf95ca7e13eb1adfb4a
H8262 <h8262@student.jamk.fi>
2016-01-23 18:12:52 +0200

    Homework and stuff :)

---
+++ Demo04/T5/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Copyright (C) Timo Heinonen
+ *
+ * This file is part of my homework :)
+ *
+ * Created: Timo Heinonen
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio = new Radio();
+            int i = 1;      //this is for the do while loop
+            
+            do {
+                Console.WriteLine("Welcome to radio! What do you want to do?");
+                Console.WriteLine("1. Turn radio on/off");
+                Console.WriteLine("2. Adjust volume");
+                Console.WriteLine("3. Adjust frequency");
+                Console.WriteLine("4. Radio status");
+                Console.WriteLine("5. Abandon radio");
+                int valikko = int.Parse(Console.ReadLine());
+                switch (valikko)
+                {
+                    case 1:
+                        {
+                            Console.Write("Set Radio on off (1 or 0): ");
+                            int valinta = int.Parse(Console.ReadLine());
+
+                            if (valinta == 1)                            
+                                radio.IsOn = true;                            
+                            else
+                                radio.IsOn = false;
+
+                            Console.WriteLine("Radio is now " + radio.IsOn);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Adjust radio volume (0-9): ");
+                            radio.Volume = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Volume is now: " + radio.Volume);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Adjust radio frequency (2000 - 26000): ");
+                            radio.Frequency = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Frequency is now: " + radio.Frequency);
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine(radio.ToString());
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Sad radio is sad :(");
+                            i = 0;
+                            break;
+                        }
+                }
+
+            } while (i == 1);
+        }
+    }
+}
---
+++ Demo04/T5/Radio.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * Copyright (C) Timo Heinonen
+ *
+ * This file is part of my homework :)
+ *
+ * Created: Timo Heinonen
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5
+{
+    class Radio
+    {
+        private int volume;
+        private int maxVolume = 9;
+        private int minVolume = 0;
+        public bool IsOn = false;
+        private int frequency;
+        private int maxFrequency = 26000;
+        private int minFrequency = 2000;
+
+
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value <= maxVolume && value >= minVolume) volume = value;
+                else
+                {
+                    if (value <= minVolume)
+                    {
+                        volume = minVolume;
+                        Console.WriteLine("Volume is set to min");
+                    }
+                    else
+                    {
+                        volume = maxVolume;
+                        Console.WriteLine("Volume is set to max");
+                    }
+                }
+            }
+        }
+
+        public int Frequency
+        {
+            get
+            {
+                return frequency;
+            }
+            set
+            {
+                if(value <= maxFrequency && value >= minFrequency)
+                {
+                    frequency = value;
+                }
+                else
+                {
+                    if(value > maxFrequency)
+                    {
+                        frequency = maxFrequency;
+                        Console.WriteLine("Frequency set to max");
+                    }
+                    else
+                    {
+                        frequency = minFrequency;
+                        Console.WriteLine("Frequency set to min");
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Frequency: " + Frequency + " Volume: " + Volume + " Is radio on? " + IsOn;
+        }
+    }
+}
